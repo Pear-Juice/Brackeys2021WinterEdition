e8186222e7099e8ada5f1023e0f2733a0b130823
hypercatw <karjalainenchristian@gmail.com>
2021-01-31 18:22:38 +0200

    add UI_Master script

---
+++ Assets/Scripts/UI_Master.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Master : MonoBehaviour
+{
+    public RawImage pyramidRendO;
+
+    // Update is called once per frame
+    void Update()
+    {
+        pyramidRendO.color = new Color(1f, 1f, 1f, Mathf.Sin(Time.time) + 1f);
+    }
+}
